1414c4a7e96da344ac38a60601ce20f06eb899ac
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-12-29 10:42:32 +0900

    タブ編集用のキーマップを追加

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,1 +1,4 @@
 vim.keymap.set('n', '<C-e>', ':NERDTreeToggle<CR>', { remap = true })
+vim.keymap.set('n', '<Tab>n', ':tabnext<CR>', { remap = true })
+vim.keymap.set('n', '<Tab>p', ':tabprevious<CR>', { remap = true })
+
